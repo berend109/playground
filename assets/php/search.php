b391bacc44696b5903cb10feb628753ebc21f9c2
berend109 <berend109@gmail.com>
2020-02-11 15:53:29 +0100

    you can now read the customworkspace.txt file

--- assets/php/search.php
+++ assets/php/search.php
@@ -2,3 +2,10 @@
 
 $directory = 'C:\\xampp\\htdocs\\workspace\\github\\berend109\\';
 $scanned_directory = array_diff(scandir($directory), array('..', '.'));
+
+$file = fopen('customworkspace.txt', 'r');
+while(! feof($file))
+  	{
+  		echo fgets($file). "<br />";
+  	}
+fclose($file);
--- assets/php/write.php
+++ assets/php/write.php
@@ -5,6 +5,7 @@ session_start();
 $filename = '../../customworkspace.txt';
 
 $content = $_GET["customWorkspace"];
+$content = ($content.PHP_EOL);
 
 file_put_contents($filename, $content, FILE_APPEND | LOCK_EX);
 
