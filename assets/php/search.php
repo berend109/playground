c5533576ee7394d80f814404255fcdec9696dc50
berend109 <berend109@gmail.com>
2020-02-18 21:05:13 +0100

    made the playground work under linux

--- assets/php/search.php
+++ assets/php/search.php
@@ -1,4 +1,14 @@
 <?php
 
+// this is for windows
+// TODO when the system is not windows throw back to linux file tree.
 $directory = 'C:\\xampp\\htdocs\\workspace\\github\\berend109\\';
-$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+
+if(is_dir($directory == true)) {
+	echo 'test';
+	$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+} else {
+	$directory = '/opt/lampp/htdocs/workspace/github/berend109/';
+	$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+	echo 'test1';
+}
