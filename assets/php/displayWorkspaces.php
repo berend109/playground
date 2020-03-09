e47fa1dcb5fbb5fea5ac3f8d27212bd3b2a869a6
berend109 <berend109@gmail.com>
2020-03-09 11:52:51 +0100

    it now shows a nice message when direcory does not exists

--- assets/php/displayWorkspaces.php
+++ assets/php/displayWorkspaces.php
@@ -2,15 +2,31 @@
 
 require_once 'get.php';
 
-$scannedDir = (getDir($con, $scannedDir));
+// first check if dir even exist that is stored in the DB.
+function checkDir($scannedDir) {
+	foreach ($scannedDir as $directory) {
+		$directory = array_shift($directory);
 
-foreach ($scannedDir as $directory) {
-	$directory = array_shift($directory);
+		$checkIfDirExist = (is_dir($directory));
+		if ($checkIfDirExist == true) {
+			displayWorkspace($directory);
+		} else {
+			echo '<br><br>';
+			echo 'Workspace not found ('. $directory. ')';
+		}
+	}
+}
 
+// if the dir exist it will display each project as a button.
+function displayWorkspace($directory) {
+	// scan for projects in the dir.
+	// also gets rids of . and .. in unix.
 	$scanned_directory = array_diff(scandir($directory), array('..', '.'));
 
+	// displays the name of the folder where the project nest in.
 	echo '<p>'. $directory. '</p>';
-	
+
+	// displays each project as a button to the screen.
 	foreach ($scanned_directory as $value) {
 		if ($value != 'playground') {
 			if ($value != '.idea') {
--- index.php
+++ index.php
@@ -12,15 +12,18 @@
 
     <div id="playground-div">
         <?php
-            require_once 'assets/php/displayWorkspaces.php';
+			require_once 'assets/php/displayWorkspaces.php';
+			
+			$scannedDir = (getDir($con, $scannedDir));
+			checkDir($scannedDir);
         ?>
-		<!-- Working code -->
-		<!-- comment because it is not needed while testing -->
-		<!-- <form  method='POST'>
+
+		<form  method='POST'>
+			<br><br>
 			<p>Add your new workspace to the playground</p>
 			Dir: <input type='text' name='input'/>
     		<button type='submit'>Submit</button>
-		</form> -->
+		</form>
 	</div>
 
 </body>
