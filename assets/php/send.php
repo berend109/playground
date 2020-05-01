38e7c7ee1465a670b104ff284d83824450fe4042
Berend Folkers <16764030+berend109@users.noreply.github.com>
2020-05-01 07:18:24 +0200

    refactor

--- assets/php/send.php
+++ assets/php/send.php
@@ -13,12 +13,12 @@ $input = addslashes($input);
 echo $input;
 
 try {
-    $sql = "INSERT INTO `projects`(`projectDir`) VALUES ('$input')";
-    $con->exec($sql);
-    echo "New record created successfully";
+	$sql = "INSERT INTO `projects`(`projectDir`) VALUES ('$input')";
+	$con->exec($sql);
+	echo "New record created successfully";
 }
 catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+	echo $sql . "<br>" . $e->getMessage();
 }
 
 $conn = null;
