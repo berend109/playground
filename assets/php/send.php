749d5f0e5f4750b2b48dc09532b2d3112e7339ae
Jeffrey Reims <jeffreims@owlin.com>
2020-06-14 13:45:32 +0200

    add bindParam to send.php

--- assets/php/send.php
+++ assets/php/send.php
@@ -5,23 +5,34 @@ require_once 'conn.php';
 $pdo = new connection;
 $con = $pdo->connect();
 
+// Prepare the statement already
+// We're going to use it later
+$stmt = $con->prepare("INSERT INTO `projects`(`projectDir`) VALUES (:input)");
+
+
+// So .. $_POST can contain anything
 $input = $_POST['input'];
 
+// This below is not safe to use for database inputs
 // windows setting
 $input = addslashes($input);
+
 
 echo $input;
 
 try {
-	$sql = "INSERT INTO `projects`(`projectDir`) VALUES ('$input')";
-	$con->exec($sql);
+	// Bind the input to the statement we prepared
+	$stmt->bindParam(':input', $input, PDO::PARAM_STR);
+	$stmt->execute();
+
 	echo "New record created successfully";
 }
 catch(PDOException $e) {
-	echo $sql . "<br>" . $e->getMessage();
+	echo "some custom error";
+	//echo $sql . "<br>" . $e->getMessage();
 }
 
-$conn = null;
+$con = null;
 
 echo '<br>';
 echo '<br>';
