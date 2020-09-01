fc67f3396d28d74eb66cef4133b88bb09a1a1577
Berend Folkers <16764030+berend109@users.noreply.github.com>
2020-09-02 00:12:43 +0200

    register
    you can now register as a user

---
+++ assets/php/login_register/register.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once('../conn.php');
+
+$pdo = new connection;
+$con = $pdo->connect();
+$name = $_POST['name'];
+$pswd = $_POST['pswd'];
+$pswd = password_hash($pswd, PASSWORD_DEFAULT);
+
+class register {
+	public function __construct() {}
+
+	public function register($con, $name, $pswd) {
+		try {
+			$stmt = $con->prepare("SELECT * FROM `users` WHERE name = ?");
+			$stmt->execute([$name]);
+			$user = $stmt->fetch();
+
+			if ($user) {
+				$_SESSION['name'] = $_POST['name'];
+				echo "Username or password allready exists";
+			} else {
+				$stmt = $con->prepare("INSERT INTO `users`(`name`, `password`) VALUES ('$name', '$pswd')");
+				$stmt->execute();
+
+				echo "register successful";
+			}
+		} catch (PDOException $e) {
+			echo "Something went wrong: ".$e->getMessage();
+		}
+	}
+}
+
+$reg = new register();
+$reg->register($con, $name, $pswd);
